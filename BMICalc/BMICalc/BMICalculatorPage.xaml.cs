873720d950a04acb4289424c5eaf251bc22a14eb
Ewa S-G <ewa.szkudzinska@gmail.com>
2023-12-01 14:45:02 +0100

    Events, Button calc BMI, Sliders

--- BMICalc/BMICalc/BMICalculatorPage.xaml.cs
+++ BMICalc/BMICalc/BMICalculatorPage.xaml.cs
@@ -6,4 +6,25 @@ public partial class BMICalculatorPage : ContentPage
 	{
         InitializeComponent();
 	}
+
+	private void TapMale_Tapped(object sender, TappedEventArgs e)
+	{
+        FrameMale.BorderColor = Color.FromArgb("#ff0000");
+        FrameFemale.BorderColor = Color.FromArgb("#474747");
+    }
+
+    private void TapFemale_Tapped(object sender, TappedEventArgs e)
+    {
+        FrameFemale.BorderColor = Color.FromArgb("#ff0000");
+        FrameMale.BorderColor = Color.FromArgb("#474747");
+    }
+
+    void BtnCalcBMI_Clicked(System.Object sender, System.EventArgs e)
+    {
+        double height = double.Parse (LblHeight.Text);
+        double weight = double.Parse(LblWeight.Text);
+
+        double bmi = (weight / height / height) * 1000;
+        DisplayAlert("Your BMI is", bmi.ToString(), "cancel");
+    }
 }
